fe9e0ea68176d47c12402ece28727e170c073c5f
br99bry <bryanblack1999@gmail.com>
2023-06-09 23:44:15 -0500

    feat: project course end

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const routerApi = require('./routes');
+const cors = require('cors');
+const {
+  logErrors,
+  boomErrorHandler,
+  errorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+const whitelist = ['http://localhost:8080', 'https://platzi.com'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('corriedo en mi puerto ' + port);
+});
